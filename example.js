09dacc7caa6dea8f8646226938bfd8b7760e55f8
Greg Bergé <neoziro@gmail.com>
2012-04-25 23:41:45 +0200

    deferred to jquery-deferred

--- example.js
+++ example.js
@@ -16,6 +16,6 @@ var config = {
 
 apnNotify = new Notify(Notify.PROTOCOL_APN, config.dev);
 apnNotify.notify({token:"b5bf61a839855b92a394dc883433df64447a7fe7ed0d8ee121f97c9973628f32", alert:"Hello World !"})
-.then(function(){
+.done(function(){
    console.log("envoi ok");
 });
