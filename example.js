7d29f2ccd406b8c0ffd9e416641250e77bb752a6
Greg Bergé <neoziro@gmail.com>
2012-04-27 14:33:35 +0200

    message send

--- example.js
+++ example.js
@@ -17,5 +17,5 @@ var config = {
 apnNotify = new Notify(Notify.PROTOCOL_APN, config.dev);
 apnNotify.notify({token:"b5bf61a839855b92a394dc883433df64447a7fe7ed0d8ee121f97c9973628f32", alert:"Hello World !"})
 .done(function(){
-   console.log("envoi ok");
+   console.log("Sending ok");
 });
