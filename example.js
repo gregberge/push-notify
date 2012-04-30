296ad87566b3504c129c768d2b72323eefa7ff25
Greg Bergé <neoziro@gmail.com>
2012-04-30 16:55:00 +0200

    remove jquery-deferred

--- example.js
+++ example.js
@@ -15,7 +15,6 @@ var config = {
     };
 
 apnNotify = new Notify(Notify.PROTOCOL_APN, config.dev);
-apnNotify.notify({token:"b5bf61a839855b92a394dc883433df64447a7fe7ed0d8ee121f97c9973628f32", alert:"Hello World !"})
-.done(function(){
+apnNotify.notify({token:"b5bf61a839855b92a394dc883433df64447a7fe7ed0d8ee121f97c9973628f32", alert:"Hello World !"}, function(){
    console.log("Sending ok");
 });
